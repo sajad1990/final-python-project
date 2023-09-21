4070eabf8f308f23ec3f8b9fdcae17e0a5833102
Angelica Ramos <135725883+Angelica2Ramos@users.noreply.github.com>
2023-09-20 22:32:57 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from second import second 
+
+app = Flask(__name__)
+app.register_blueprint(second, url_prefix="")
+
+@app.route("/")
+def test():
+    return render_template("base.html")
+
+'if__name__'== "__main__"
+app.run(debug=True)
